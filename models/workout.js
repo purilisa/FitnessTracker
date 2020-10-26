ee06426adb38df13fd826c30eee213d8d1d1c506
LisaSuv <63619744+purilisa@users.noreply.github.com>
2020-10-25 21:31:15 -0400

    add fitness tracker changes and files

---
+++ models/workout.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date()
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter excercise"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter excercise"
+      },
+      duration: {
+        type: Number,
+        required: "Enter excercise"
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
+}, {
+  toJSON: {
+    virtuals: true
+  }
+});
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration
+  }, 0)
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
